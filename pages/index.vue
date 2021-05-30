a0ff6a8f42aa87b4e383210e46215b8105b89238
Jerick C. Mangalus <jmangaluz@gmail.com>
2021-05-30 21:46:56 +0800

    initial

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-row fluid class="green">1</v-row>
+    <v-row fluid class="red">2</v-row>
+    <v-row fluid class="green">3</v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
